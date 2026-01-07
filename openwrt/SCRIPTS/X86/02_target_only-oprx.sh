d674438ec8571ff539d18a859fd000320f5ba3d7
ilxp <lixp@live.com>
2026-01-07 21:05:36 +0800

    Update 02_target_only-oprx.sh

--- openwrt/SCRIPTS/X86/02_target_only-oprx.sh
+++ openwrt/SCRIPTS/X86/02_target_only-oprx.sh
@@ -922,6 +922,10 @@ merge_package master https://github.com/QiuSimons/OpenWrt-Add.git package/new le
 
 #fanchmwrt的
 git clone https://github.com/fanchmwrt/fanchmwrt-packages.git package/fcm-packages
+#更新特征库
+wget -qO- https://github.com/ilxp/oaf/raw/main/feature.cfg > package/base-files/files/etc/fwxd/feature.cfg
+chmod +x package/base-files/files/etc/fwxd/feature.cfg
+
 merge_package fanchmwrt-24.10.4 https://github.com/fanchmwrt/fanchmwrt.git package package/fcm
 sed -i "s/+luci-i18n-base-zh-cn/ /g" package/fcm/luci-theme-fanchmwrt/Makefile
 rm -rf target/linux/generic/hack-6.6/980-nf-contrack-support-fwx-data.patch
